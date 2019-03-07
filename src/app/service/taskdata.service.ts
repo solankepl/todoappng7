af76b1c8631a84ec994e6a0051971f152d9b4ceb
Pavan Solanke <solankepl@gmail.com>
2019-03-07 11:09:25 +0530

    Update taskdata.service.ts
    load task from url

--- src/app/service/taskdata.service.ts
+++ src/app/service/taskdata.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskdataService {
 
-  constructor() { }
+private _issuesUrl = '//jsonplaceholder.typicode.com/todos';
+  private httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+  };
+
+  constructor(private _http: HttpClient) { }
+
+  getTaskList() {
+      return this._http.get(this._issuesUrl);
+  }
+  
 }
+
