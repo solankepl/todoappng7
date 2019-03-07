3e669aa037e640977266c6eb56057d18d0f36e70
Pavan Solanke <solankepl@gmail.com>
2019-03-07 15:45:41 +0530

    Update app.module.ts
    import filter app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { AddtaskComponent } from './addtask/addtask.component';
 import { ViewtaskComponent } from './viewtask/viewtask.component';
 import { DeletedtaskComponent } from './deletedtask/deletedtask.component';
+import { ByfieldPipe } from './shared/byfield.pipe';
+
 
 @NgModule({
   declarations: [
@@ -13,10 +17,13 @@ import { DeletedtaskComponent } from './deletedtask/deletedtask.component';
     ContainerComponent,
     AddtaskComponent,
     ViewtaskComponent,
-    DeletedtaskComponent
+    DeletedtaskComponent,
+    ByfieldPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
