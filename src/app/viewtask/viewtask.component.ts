4c9365195aa14f41684e9ff03ca74844097b3d39
Pavan Solanke <solankepl@gmail.com>
2019-03-07 11:08:18 +0530

    Update viewtask.component.ts
    load task from api

--- src/app/viewtask/viewtask.component.ts
+++ src/app/viewtask/viewtask.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TaskdataService } from '../service/taskdata.service';
 @Component({
   selector: 'app-viewtask',
   templateUrl: './viewtask.component.html',
   styleUrls: ['./viewtask.component.scss']
 })
 export class ViewtaskComponent implements OnInit {
-
-  constructor() { }
+  private taskData:Array<any>;
+  constructor(private taskDataService:TaskdataService) { }
 
   ngOnInit() {
+    this.getIssuesList();
+  }
+
+  getIssuesList() {
+    this.taskDataService.getTaskList().subscribe(
+      (task:any) => {
+         this.taskData = task;
+      },
+      err => console.log(err)
+    );
   }
 
 }
