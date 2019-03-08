6fe13000ea4fcd7c69f34fb402addae487614de8
Pavan Solanke <solankepl@gmail.com>
2019-03-08 12:33:01 +0530

    Update viewtask.component.ts
    add list to completed task

--- src/app/viewtask/viewtask.component.ts
+++ src/app/viewtask/viewtask.component.ts
@@ -6,8 +6,11 @@ import { TaskdataService } from '../service/taskdata.service';
   styleUrls: ['./viewtask.component.scss']
 })
 export class ViewtaskComponent implements OnInit {
-  private taskData:Array<any>;
-  constructor(private taskDataService:TaskdataService) { }
+  private taskData: Array<any>;
+  private complatedTask: Array<any>;
+  private pendingTask: Array<any>;
+  private markaAsComplatedTask: Array<number> = new Array();
+  constructor(private taskDataService: TaskdataService) { }
 
   ngOnInit() {
     this.getTasksList();
@@ -15,11 +18,52 @@ export class ViewtaskComponent implements OnInit {
 
   getTasksList() {
     this.taskDataService.getTaskList().subscribe(
-      (task:any) => {
-         this.taskData = task;
+      (task: any) => {
+        this.taskData = task;
+        this.filterTaskList();
       },
       err => console.log(err)
     );
+  }
+
+  filterTaskList() {
+    this.complatedTask = this.taskData.filter(task => task.completed === true);
+    this.pendingTask = this.taskData.filter(task => task.completed === false);
+  }
+
+setAllComplatedTask(evt, list){
+  //this.pendingTask = this.taskData.map(task => task.completed = true);
+  let setSelected = evt.target.checked
+  
+  list.forEach(function (task) {
+           task.completed = setSelected;
+  });
+    
+}
+
+  setComplatedTask(evt, task) {
+    if (evt.target.checked) {
+      this.markaAsComplatedTask.push(task.id);
+    } else {
+      let index = this.markaAsComplatedTask.indexOf(task.id, 0);
+      if (index > -1) {
+        this.markaAsComplatedTask.splice(index, 1);
+      }
+    }
+    task.completed = evt.target.checked;
+
+  }
+
+  markAsComplated() {
+    this.filterTaskList();
+    /*let selectedTaskArray = this.markaAsComplatedTask;
+    this.taskData.forEach(function (task) {
+      for (let i of selectedTaskArray) {
+       if(task.id === i) {
+           task.completed = true;
+        }
+      }
+    });*/
   }
 
 }
